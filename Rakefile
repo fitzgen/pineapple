0cf6f7bef4ee6b4aa89d5f0dc186b1a4a6421f91
Nick Fitzgerald <fitzgen@gmail.com>
2010-10-16 14:28:58 -0700

    Moving away form pubsub to events that can be mixed in to other prototypes

--- Rakefile
+++ Rakefile
@@ -1,10 +1,9 @@
-files = [ "intro", "pubsub", "routes", "modules", "outro" ]
+files = [ "intro", "events", "routes", "views", "models", "outro" ]
 
 task :default => :pineapple
 
 task :pineapple do
 	sh "mkdir -p dist"
-
 	sh "cat " + files.map {|file| "src/" + file + ".js"}.join(" ") +
 		" > dist/pineapple.js"
 end
---
+++ src/events.js
@@ -0,0 +1,45 @@
+// ### Events
+
+var eventEmitter = {
+    bind: function (event, callback) {
+        this._eventHandlers = this._eventHandlers        || {};
+        var handlers        = this._eventHandlers[event] || (this._eventHandlers[event] = []);
+        handlers.push(callback);
+        return this;
+    },
+    unbind: function (event, callback) {
+        var handlers, i, len;
+        switch (arguments.length) {
+        case 0:
+            delete this._eventHandlers;
+            return this;
+        case 1:
+            this._eventHandlers = this._eventHandlers || {};
+            delete this._eventHandlers[event];
+            return this;
+        case 2:
+            this._eventHandlers = this._eventHandlers        || {};
+            handlers            = this._eventHandlers[event] || [];
+            for (i = 0, len = handlers.length; i++) {
+                if (handlers[i] === callback) {
+                    handlers.splice(i, 1);
+                    break;
+                }
+            }
+            return this;
+        default:
+            throw new Error("Too many arguments to `unbind`: " + arguments.length);
+        }
+    }
+    trigger: function (event) {
+        if ( ! this._eventHandlers[event] )
+            return;
+        var args = slice(arugments, 1),
+            self = this;
+        for ( var i = 0, len = this._eventHandlers[event].length; i < len; i++ )
+            async(function () {
+                self._eventHandlers[event][i].apply({}, args);
+            });
+        return this;
+    }
+};
