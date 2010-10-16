f1bf6e74e1369f5d555263be84ab1f43069a229d
Nick Fitzgerald <fitzgen@gmail.com>
2010-10-16 15:34:41 -0700

    Updating intro

--- src/intro.js
+++ src/intro.js
@@ -1,5 +1,5 @@
 window["pineapple"] = function () {
-    if ( !(this instanceof window.pineapple) )
+    if ( ! (this instanceof window.pineapple) )
         return new window.pineapple();
 
     // ### Private methods and variables
@@ -8,4 +8,34 @@ window["pineapple"] = function () {
 
     function async (f) {
         setTimeout(f, 20);
+    }
+
+    function slice (obj, i) {
+        return Array.prototype.slice.call(obj, i || 0);
+    }
+
+    function mixin (target, props) {
+        for (var k in props)
+            if (props.hasOwnProperty(k))
+                target[k] = props[k];
+    }
+
+    // Borrowed from Backbone.js, Copyright Jeremy Ashkenas and Document Cloud,
+    // MIT licensed.
+    function inherits (parent, protoProps, ctorProps) {
+        var child = protoProps.hasOwnProperty('constructor')
+            ? protoProps.constructor
+            : function () {
+                return parent.apply(this, arguments);
+            };
+
+        var ctor = function () {};
+        ctor.prototype = parent.prototype;
+        child.prototype = new ctor();
+
+        mixin(child.prototype, protoProps);
+        ctorProps && mixin(child, ctorProps);
+        child.prototype.constructor = child;
+
+        return child;
     }
