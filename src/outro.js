db78744afc078e278d745410031f787c9d397fc4
Nick Fitzgerald <fitzgen@gmail.com>
2010-10-16 15:37:16 -0700

    Deleting the global references to jQuery

--- src/outro.js
+++ src/outro.js
@@ -1,4 +1,10 @@
-    $(document).ready(function () {
-        app.publish("ready");
-    });
+$(document).ready(function () {
+    app.publish("ready");
+});
+
+// Delete global references to jQuery so people can't cheat their way out of the
+// framework (except when testing).
+(window.the && window.should) || delete window.$;
+(window.the && window.should) || delete window.jQuery;
+
 };
