eef785ef6f99d91cfb4d6c9d8fb4fa937e26067b
Nick Fitzgerald <fitzgen@gmail.com>
2010-10-18 00:10:43 -0700

    Adding a bunch of library views for common form inputs

--- src/views.js
+++ src/views.js
@@ -136,4 +136,98 @@
         return view;
     };
 
+    // ### Pre-defined, Library Views
+
+    // For subclassing only.
+    var Input = app.defineView({
+        defaultElement: "input",
+        value: "",
+        initialize: function (self, opts) {
+            self.events = opts.events || {};
+            self.name = opts.name || "";
+            opts.value && (self.value = opts.value);
+            self.input = self.el;
+            self.$(self.el).attr({
+                type: self.inputType,
+                name: self.name,
+                placeholder: opts.placeholder || ""
+            });
+            if (opts.label) {
+                self.el = self.$("<label>").text(opts.label).append(self.input)[0];
+            }
+        },
+        delegate: function (self) {
+            // Handle the cases in which events are being delegated directly to
+            // the object, but we are automagically creating <label>s around the
+            // inputs, so we must modify the selector.
+            //
+            //     ( "keyup" => "keyup input" )
+            var newEvents;
+            if (self.el.tagName === "label") {
+                for (var key in self.events) {
+                    if (self.events.hasOwnProperty(key)) {
+                        newEvents[key.match(/^\w+$/) ? key + " input" : key] = self.events[key];
+                    }
+                }
+            }
+            self.events = newEvents;
+            return BaseView.prototype.delegate.call(self);
+        },
+        activate: function (self, val) {
+            BaseView.prototype.activate.call(self);
+            self.$(self.input).val(val || this.value);
+            return this;
+        },
+        deactivate: function (self) {
+            BaseView.prototype.deactivate.call(self);
+            self.$(self.input).val("");
+            return this;
+        },
+        disable: function (self) {
+            self.$(self.input).attr("disabled", "disabled");
+            return this;
+        },
+        enable: function (self) {
+            self.$(self.input).removeAttr("disabled");
+            return this;
+        },
+        valueOf: function (self) {
+            return self.$(self.input).val();
+        }
+    });
+
+    var TextInput = app.defineView("TextInput", Input, {
+        inputType: "text"
+    });
+
+    var PasswordInput = app.defineView("PasswordInput", Input, {
+        inputType: "password"
+    });
+
+    var FileInput = app.defineView("FileInput", Input, {
+        inputType: "file"
+    });
+
+    var Button = app.defineView("Button", Input, {
+        inputType: "button",
+        initialize: function (self, opts) {
+            // For buttons, the label is the value, no need to make a <label>.
+            opts.value = opts.label;
+            delete opts.label;
+            Input.prototype.initialize.call(self);
+        }
+    });
+
+    var SubmitButton = app.defineView("SubmitButton", Button, {
+        inputType: "submit",
+        label: "Submit"
+    });
+
+    var ResetButton = app.defineView("ResetButton", Button, {
+        inputType: "reset",
+        label: "Reset"
+    });
+
+    var RadioInput; // TODO: private RadioInput for making radio lists.
+
 }());
