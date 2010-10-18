282bdcf32dc3ef36d9b619118fc6982df3d99533
Nick Fitzgerald <fitzgen@gmail.com>
2010-10-17 22:50:15 -0700

    Don't remove jQuery, lots of useful helpers, and it is pointless to try and force the user in to working within a sandbox in the views. If they want to get out they will, so the best I can do is encourage good practices.

--- src/outro.js
+++ src/outro.js
@@ -10,9 +10,4 @@ $(document).ready(function () {
         onReadys.pop()();
 });
 
-// Delete global references to jQuery so people can't cheat their way out of the
-// framework (except when testing).
-(window.the && window.should) || delete window.$;
-(window.the && window.should) || delete window.jQuery;
-
 };
